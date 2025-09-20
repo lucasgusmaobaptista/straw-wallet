fb8ba5e24f2108d9660858e03235ebb48c11146f
Lucas <lucasgusmaobaptista@gmail.com>
2025-09-20 12:54:29 -0300

    first commit

---
+++ straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/StrawWalletApiApplication.java
@@ -0,0 +1,13 @@
+package me.lucasgusmao.straw_wallet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrawWalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrawWalletApiApplication.class, args);
+	}
+
+}
---
+++ straw-wallet-api/src/test/java/me/lucasgusmao/straw_wallet_api/StrawWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lucasgusmao.straw_wallet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrawWalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
