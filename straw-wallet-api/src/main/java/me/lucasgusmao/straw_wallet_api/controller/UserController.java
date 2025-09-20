5f9408d015a8022457a9366e80be0c4965559b99
Lucas <lucasgusmaobaptista@gmail.com>
2025-09-20 19:07:35 -0300

    Implement Endpoint for activating account

--- straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/controller/UserController.java
+++ straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/controller/UserController.java
@@ -5,9 +5,7 @@ import me.lucasgusmao.straw_wallet_api.dto.UserDTO;
 import me.lucasgusmao.straw_wallet_api.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +17,15 @@ public class UserController {
     public ResponseEntity<UserDTO> register(@RequestBody UserDTO dto) {
         UserDTO user = service.register(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/activate")
+    public ResponseEntity<String> activateProfile(@RequestParam String token) {
+        boolean result = service.activateProfile(token);
+        if (result) {
+            return ResponseEntity.status(HttpStatus.OK).body("Conta ativada com sucesso!");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Erro ao ativar conta: Token não encontrado");
+        }
     }
 }
