ad8f528838ea21ad32b29ab9b6cd7c456161b04a
Lucas <lucasgusmaobaptista@gmail.com>
2025-09-20 17:46:08 -0300

    Create UserRepository

---
+++ straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package me.lucasgusmao.straw_wallet_api.repository;
+
+import me.lucasgusmao.straw_wallet_api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    Optional<User> existsUserByUsername(String username);
+    Optional<User> existsUserByEmail(String email);
+}
