e8449c55ce8cb2358b59bd629e193a7d86daaf72
Lucas <lucasgusmaobaptista@gmail.com>
2025-09-20 17:45:58 -0300

    Create User entity

---
+++ straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/model/User.java
@@ -0,0 +1,68 @@
+package me.lucasgusmao.straw_wallet_api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "username", unique = true, length = 50)
+    private String username;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 30)
+    private String password;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+    @Column(name = "activation_token")
+    private String activationToken;
+
+    @PrePersist
+    public void prePersist() {
+        if (this.isActive == null) {
+            isActive = false;
+        }
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
