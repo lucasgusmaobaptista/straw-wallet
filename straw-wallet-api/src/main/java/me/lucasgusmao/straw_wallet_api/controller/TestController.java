77d2916aa4d4c69cef976827bd86b9f9f060f690
Lucas <lucasgusmaobaptista@gmail.com>
2025-09-20 17:46:39 -0300

    Add a Endpoint to test application

---
+++ straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/controller/TestController.java
@@ -0,0 +1,16 @@
+package me.lucasgusmao.straw_wallet_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"test", "status"})
+public class TestController {
+
+    // Simple endpoint to verify that the application is running
+    @GetMapping
+    public String testEndpoint() {
+        return "Test successful! Application is running.";
+    }
+}
