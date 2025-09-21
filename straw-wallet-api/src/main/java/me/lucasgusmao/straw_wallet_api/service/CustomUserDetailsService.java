485324d924a8cc8ddb6326a23d0c3afe8403e89c
Lucas <lucasgusmaobaptista@gmail.com>
2025-09-21 12:03:27 -0300

    Implement UserDetailsService on CustomUserDetailsService

---
+++ straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package me.lucasgusmao.straw_wallet_api.service;
+
+import lombok.RequiredArgsConstructor;
+import me.lucasgusmao.straw_wallet_api.model.User;
+import me.lucasgusmao.straw_wallet_api.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário: " + username + " não encontrado"));
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities(Collections.emptyList())
+                .build();
+    }
+}
