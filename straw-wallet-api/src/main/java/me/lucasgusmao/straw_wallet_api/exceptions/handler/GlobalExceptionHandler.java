fa84cb36fd7c1fb473aa3c3c88fb7b1196645157
Lucas <lucasgusmaobaptista@gmail.com>
2025-09-21 16:41:06 -0300

    Add defaults exceptions handlers

--- straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/exceptions/handler/GlobalExceptionHandler.java
+++ straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/exceptions/handler/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package me.lucasgusmao.straw_wallet_api.exceptions.handler;
 
+import jakarta.persistence.EntityNotFoundException;
 import me.lucasgusmao.straw_wallet_api.exceptions.custom.AlreadyExistsException;
 import me.lucasgusmao.straw_wallet_api.exceptions.custom.EmailSendException;
 import me.lucasgusmao.straw_wallet_api.exceptions.custom.InvalidCredentialsException;
 import me.lucasgusmao.straw_wallet_api.exceptions.response.ResponseError;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,6 +16,18 @@ import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseError handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return new ResponseError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), List.of());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseError handleEntityNotFoundException(EntityNotFoundException e) {
+        return new ResponseError(HttpStatus.NOT_FOUND.value(), e.getMessage(), List.of());
+    }
 
     @ExceptionHandler(AlreadyExistsException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
@@ -37,4 +51,5 @@ public class GlobalExceptionHandler {
     public ResponseError handleInvalidCredentials(InvalidCredentialsException e) {
         return new ResponseError(HttpStatus.BAD_REQUEST.value(), "Credenciais inválidas: " + e.getMessage(), List.of());
     }
+
 }
