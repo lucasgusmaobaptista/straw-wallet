0351fc0b9e179075cc05c82861c74079a593bfd0
Lucas <lucasgusmaobaptista@gmail.com>
2025-09-21 17:03:32 -0300

    Implement Endpoint for finding categories by current user logged in

--- straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/controller/CategoryController.java
+++ straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/controller/CategoryController.java
@@ -6,10 +6,9 @@ import me.lucasgusmao.straw_wallet_api.dto.CategoryDTO;
 import me.lucasgusmao.straw_wallet_api.service.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
@@ -23,4 +22,11 @@ public class CategoryController {
         CategoryDTO savedCategory = service.save(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
     }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> getCategories() {
+        List<CategoryDTO> categories = service.findCurrentUserCategories();
+        return ResponseEntity.ok().body(categories);
+    }
 }
+
