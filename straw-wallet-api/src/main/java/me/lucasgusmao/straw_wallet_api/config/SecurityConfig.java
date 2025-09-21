a1b0290a2f0cfdc285e9f8d73a8315e086fbc1fb
Lucas <lucasgusmaobaptista@gmail.com>
2025-09-21 14:12:12 -0300

    Add JWT to application

--- straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/config/SecurityConfig.java
+++ straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package me.lucasgusmao.straw_wallet_api.config;
 
 import lombok.RequiredArgsConstructor;
+import me.lucasgusmao.straw_wallet_api.security.JwtRequestFilter;
 import me.lucasgusmao.straw_wallet_api.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -26,6 +28,7 @@ import java.util.List;
 public class SecurityConfig {
 
     private final CustomUserDetailsService userDetailsService;
+    private final JwtRequestFilter jwtRequestFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -35,6 +38,7 @@ public class SecurityConfig {
                         .requestMatchers("/status", "/test", "/register", "/activate", "/login" ).permitAll()
                         .anyRequest().authenticated())
                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+               .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
                .build();
     }
 
