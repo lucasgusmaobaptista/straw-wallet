5bfc953d0b7fb97c7469d31a72e7b70db6be88f9
Lucas <lucasgusmaobaptista@gmail.com>
2025-09-21 12:04:20 -0300

    Add Login endpoint

--- straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/controller/UserController.java
+++ straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/controller/UserController.java
@@ -1,11 +1,15 @@
 package me.lucasgusmao.straw_wallet_api.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import me.lucasgusmao.straw_wallet_api.dto.AuthDTO;
 import me.lucasgusmao.straw_wallet_api.dto.UserDTO;
 import me.lucasgusmao.straw_wallet_api.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,7 +18,7 @@ public class UserController {
     private final UserService service;
 
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> register(@RequestBody UserDTO dto) {
+    public ResponseEntity<UserDTO> register(@RequestBody @Valid UserDTO dto) {
         UserDTO user = service.register(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
@@ -26,6 +30,20 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.OK).body("Conta ativada com sucesso!");
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Erro ao ativar conta: Token não encontrado");
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, Object>> login(@RequestBody @Valid AuthDTO dto) {
+        try {
+            if (!service.isUserActive(dto.username())) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body(
+                        Map.of("error", "O usuário informado não foi ativado. Por favor, verifique seu e-mail e ative sua conta."));
+            }
+            Map<String, Object> response = service.authenticateAndGenerateToken(dto);
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", e.getMessage()));
         }
     }
 }
