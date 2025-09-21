d4ea4fd3cb8db5006e97309237f3bc89bfb38698
Lucas <lucasgusmaobaptista@gmail.com>
2025-09-21 17:28:23 -0300

    Add Endpoint for get the current user categories by a specific type

--- straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/controller/CategoryController.java
+++ straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/controller/CategoryController.java
@@ -3,6 +3,7 @@ package me.lucasgusmao.straw_wallet_api.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import me.lucasgusmao.straw_wallet_api.dto.CategoryDTO;
+import me.lucasgusmao.straw_wallet_api.model.CategoryType;
 import me.lucasgusmao.straw_wallet_api.service.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +27,13 @@ public class CategoryController {
     @GetMapping
     public ResponseEntity<List<CategoryDTO>> getCategories() {
         List<CategoryDTO> categories = service.findCurrentUserCategories();
+        return ResponseEntity.ok().body(categories);
+    }
+
+    @GetMapping("/{type}")
+    public ResponseEntity<List<CategoryDTO>> getCurrentUserCategoriesByType(@PathVariable String type) {
+        CategoryType categoryType = CategoryType.valueOf(type.toUpperCase());
+        List<CategoryDTO> categories = service.findCurrentUserCategoriesByType(categoryType);
         return ResponseEntity.ok().body(categories);
     }
 }
