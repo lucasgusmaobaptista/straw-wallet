dd521a1d2eb2aca294c6ff0844dc67b0cffc83d9
Lucas <lucasgusmaobaptista@gmail.com>
2025-09-24 16:37:39 -0300

    Create Expense and Income entities

--- straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/model/Expense.java
+++ straw-wallet-api/src/main/java/me/lucasgusmao/straw_wallet_api/model/Expense.java
@@ -1,4 +1,58 @@
 package me.lucasgusmao.straw_wallet_api.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "expenses")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "icon")
+    private String icon;
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Column(name = "amount")
+    private BigDecimal amount;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @PrePersist
+    public void prePersist() {
+        if (this.createdAt == null) this.createdAt = LocalDateTime.now();
+        if (this.date == null) this.date = LocalDate.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() { this.updatedAt = LocalDateTime.now(); }
 }
